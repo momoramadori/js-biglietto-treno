5faba48f571f3bc1e12ca47a74bad973af603778
Momo Ramadori <momoramadori8@gmail.com>
2020-04-20 15:18:15 +0200

    leggermente stilato

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 
 //Numero chilometri che intende percorrere
 var km = prompt('Quanti km devi percorrere?');
+document.getElementById('km').innerHTML = ("Devi coprire: " + km + ' km')
 
 //Età del passeggero
 var age = prompt('Quanti hanni hai?');
-
+document.getElementById('età').innerHTML = ("La tua età: " + age + ' anni')
 //Calcolo del prezzo del Biglietto
 
 if ( age < 18 ) {
@@ -16,4 +17,4 @@ if ( age < 18 ) {
     var ticketPrice = (km * 0.21);
 }
 
-document.getElementById('prezzo').innerHTML = ( "Il prezzo del Biglietto è: € " + ticketPrice );
+document.getElementById('prezzo').innerHTML = ( "Il prezzo del biglietto è: € " + ticketPrice );
